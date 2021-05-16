99eecf80fc30d6a4b37c8e77c4e15f7a770cb8ad
= <=>
2021-05-16 11:05:30 -0500

    Initial commit

--- accidents/R/accident_package.R
+++ accidents/R/accident_package.R
@@ -1,3 +1,67 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:183f79a353f15f18411e0e275d6e135d127df736246c439dd285dcb4f5bfc622
-size 1319
+#reading in the accidents data
+accidentsData <- get(load("data/US_Accidents_Dec20.RData"))
+
+#projection of the latitude-longitude points
+projcrs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+
+#returns the names of all of the places with accident data
+#these names could be used, when splitting the names by ", "
+#' getPlaceNames
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' getPlaceNames()
+getPlaceNames <- function(){
+  return(unlist(accidentsData$`City, County, State`))
+}
+
+#retrieve the available accidents data,
+#given a city, county, state names as input
+#' getPlaceDataAsList
+#'
+#' @param city city name
+#' @param county county name
+#' @param state state name
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' getPlaceDataAsList("Chicago", "Cook", "IL")
+#' getPlaceDataAsList("city", "county", "state")
+getPlaceDataAsList <- function(city, county, state){
+  return(accidentsData[accidentsData$City==city &
+                         accidentsData$County==county &
+                         accidentsData$State==state,])
+}
+
+#retrieve the available accidents data,
+#given a city, county, state names as input,
+#in the form of points that could be mapped
+#' getPlaceDataAsPoints
+#'
+#' @param city city name
+#' @param county county name
+#' @param state state name
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' getPlaceDataAsPoints("Chicago", "Cook", "IL")
+#' getPlaceDataAsPoints("city", "county", "state")
+getPlaceDataAsPoints <- function(city, county, state){
+  data_df <- data.frame(accidentsData[accidentsData$City==city &
+                                        accidentsData$County==county &
+                                        accidentsData$State==state,])
+  if(nrow(data_df) > 0){
+    points <- sf::st_as_sf(x = data_df, coords = c("Start_Lng", "Start_Lat"), crs = projcrs)
+    return(points)
+  }else{
+    return("No points due to no data for input place")
+  }
+}
+
+
---
+++ accidents/README.md
@@ -0,0 +1,31 @@
+
+# accidents
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+I made this R package to enable easy information retrieval regarding US car accident (2016-2020) data. This package was made as part of an assignment.
+
+Dataset: https://www.kaggle.com/sobhanmoosavi/us-accidents
+
+The purpose of the package, as stated earlier, is to make it easier to retrieve data from a US car accidents dataset that I am currently using for the project. The dataset has a large file size, making reading operations somewhat time consuming. As a result I decided to simplify that process by making it easy to retrieve a specific subsection of that dataset, depending what specific place the user wants to look at (getPlaceDataAsList(city, county, state)). This dataset subsection can also be retrieved and then converted into points (getPlaceDataAsPoints(city, county, state)). Additionally, through modifying the dataset's place name structure, I made the package be able to return the total list of places the dataset has accident data for (through getPlaceNames()), to make the place data retrieval process easier. Using this package is for educational purposes and non commercial use only.
+
+## Installation
+
+To install do the following command: 
+
+install_github("michaelyohannes123/accidents_package", subdir="accidents")
+
+``` r
+install.packages("accidents")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(accidents)
+getPlaceDataAsList("Chicago", "Cook", "IL")
+```
+
